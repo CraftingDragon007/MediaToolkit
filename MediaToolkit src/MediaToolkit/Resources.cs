f1d891c442c8b6222ab62fd01a419a92e6231677
CraftingDragon007 <craftingdragon007@outlook.com>
2022-11-27 15:49:36 +0100

    Raised Package Version, because of new Nuget release (netstandard 2.1)

--- MediaToolkit src/MediaToolkit/Resources.cs
+++ MediaToolkit src/MediaToolkit/Resources.cs
@@ -6,7 +6,7 @@ namespace MediaToolkit
         public static string ExceptionsFFmpegProcessNotRunning => "FFmpeg process is not running.";
         public static string ExceptionMediaInputFileNotFound => "Input file not found";
         public static string ExceptionsNullFFmpegGzipStream => "FFMpeg GZip stream is null";
-        public static string? FFmpegProcessName { get; set; }
+        public static string? FFmpegProcessName { get; set; } = "ffmpeg";
         public static string? FFmpegManifestResourceName { get; set; }
     }
 }
